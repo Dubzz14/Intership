6eaa078936b17e3a88686066255b576188c24da0
Dubzz14 <dubbyezeh@gmail.com>
2025-07-09 14:30:21 +0100

    Refactor BankAccount class to enhance transaction handling and maintain transaction history

--- Intro.py
+++ Intro.py
@@ -3,23 +3,31 @@ class BankAccount:
         self.Account_number = Account_number
         self.name = name
         self.amount = amount
+        self.transaction_history = []
 
     def withdraw(self, w_amount):
         if w_amount > self.amount:
             print("Insuffisient funds")
         else:
             self.amount = self.amount - w_amount
+            self.transaction_history.append(f"Withdrawal: {w_amount}")
+            self.store_transaction(self.name, self.amount, "Withdrawal", self.Account_number)
             return (f'You have successfully withdrawn {w_amount}')
         
     
     def deposit(self, d_amount):
         print("Your account does not support this transaction")
         self.amount = d_amount + self.amount
+        self.transaction_history.append(f"Deposit: {d_amount}")
+        self.store_transaction(self.name, self.amount, "Deposit", self.Account_number)
         return (f'You have successfully Deposited {d_amount}')
     
     def check_balance(self):
         return self.amount
     
+    def store_transaction(self, name, amount, transaction_type, account_number):
+        self.transaction_history.append({"name":name, "amount":amount, "transaction type": transaction_type, "account number": account_number})
+
 david_ac1 = BankAccount(22233456, 'David', 35000)
 david_ac2 = BankAccount(23234567, 'David', 45000)
 
@@ -38,6 +46,8 @@ print(david_ac2.deposit(500000))
 print(david_ac1.check_balance())
 print(david_ac2.check_balance())
 
+print("The transaction history for david_ac1:", david_ac1.transaction_history)
+print("The transaction history for david_ac2:", david_ac2.transaction_history)
 
 class Retirement_fund(BankAccount):
     def __init__(self, retirement_age, amount, name, Account_number, pension_type):
@@ -57,7 +67,5 @@ Ngozi_ci2 = Retirement_fund(55,65000, 'Ngozi', 22334567, None  )
 print (Ngozi_ci1.check_retirement_age())
 print (Ngozi_ci2.check_retirement_age())
 
-transaction_history = []
 
-def store_transaction(name, amount, transaction_type, account_number):
-    transaction_history.append({"name":name, "amount":amount, "transaction type": transaction_type, "account number": account_number})
+
