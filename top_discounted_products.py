373072004f38f075d85e7245c7b8e45fcb4c87be
Dubzz14 <dubbyezeh@gmail.com>
2026-01-13 02:16:05 +0100

    feat: Implement database connection using environment variables in top_discounted_products.py

--- top_discounted_products.py
+++ top_discounted_products.py
@@ -1,20 +1,44 @@
-import os
-import psycopg2
 from flask import Flask, jsonify
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 app = Flask(__name__)
 
-DB_HOST = os.getenv("DB_HOST","localhost")
-DB_NAME = os.getenv("DB_NAME","Execute")
-DB_USER = os.getenv("DB_USER","postgres")
-DB_PASS = os.getenv("DB_PASS","ezeh")
-
 def get_db_connection():
-    conn = psycopg2.connect(
-        host=DB_HOST,
-        database=DB_NAME,
-        user=DB_USER,
-        password=DB_PASS
+    return psycopg2.connect(
+        host="localhost",
+        database="trial", 
+        user="postgres",
+        password="ezeh",
+        cursor_factory=RealDictCursor 
     )
-    return conn
 
+QUERY_1 = """
+--Top discounted products
+
+SELECT title, "discountPercentage"
+FROM products_data
+ORDER BY "discountPercentage" DESC
+LIMIT 10; 
+"""
+
+@app.route('/category-analysis')
+def get_analysis():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        
+        cur.execute(QUERY_1)
+        
+        results = cur.fetchall()
+        
+        cur.close()
+        conn.close()
+        
+        return jsonify(results)
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
